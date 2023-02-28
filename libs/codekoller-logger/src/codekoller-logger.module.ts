e8372f2e26292ac20f252df61f6ab43a7877b933
William Koller <williamkoller404@gmail.com>
2023-02-27 21:11:23 -0300

    chore: add new version 0.0.3

--- libs/codekoller-logger/src/codekoller-logger.module.ts
+++ libs/codekoller-logger/src/codekoller-logger.module.ts
@@ -1,8 +1,16 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { CodekollerLoggerService } from './codekoller-logger.service';
 
 @Module({
   providers: [CodekollerLoggerService],
   exports: [CodekollerLoggerService],
 })
-export class CodekollerLoggerModule {}
+export class CodekollerLoggerModule {
+  static register(): DynamicModule {
+    return {
+      module: CodekollerLoggerModule,
+      providers: [CodekollerLoggerService],
+      exports: [CodekollerLoggerService],
+    };
+  }
+}
